9167de0c1dfb7875726c8f9819ec8f1751ae5052
v.a.ilukhin@mail.ru <v.a.ilukhin@mail.ru>
2020-04-07 08:34:54 +0300

    Разделил проект на UI и SDK и добавил конфиг для хранения данных тестового пользователя

---
+++ UI/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string username = ConfigurationManager.AppSettings["UsernameForTest"];
+            string password = ConfigurationManager.AppSettings["PasswordForTest"];
+            long appid = long.Parse( ConfigurationManager.AppSettings["AppIdForTest"]);
+        }
+    }
+}
