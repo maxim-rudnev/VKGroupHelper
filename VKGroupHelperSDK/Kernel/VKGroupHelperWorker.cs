aa8a92b0011edb348c2cc38e2099366cdce30ab9
Алексей Шувалов <egor.alex@inbox.ru>
2020-11-16 14:46:32 +0300

    Enter and

--- VKGroupHelperSDK/Kernel/VKGroupHelperWorker.cs
+++ VKGroupHelperSDK/Kernel/VKGroupHelperWorker.cs
@@ -115,7 +115,7 @@ namespace VKGroupHelperSDK.Kernel
                     }
                 }
             }
-
+            // 
             // загрузка видео
             if (contentInfo.IsVideo())
             {
